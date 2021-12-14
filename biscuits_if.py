f46a89c3f75ab4d4997e6504ef7655843d8b6614
Albert Ramos <albertramos.dev@gmail.com>
2021-12-14 12:04:40 -0700

    Update biscuits_if.py

--- biscuits_if.py
+++ biscuits_if.py
@@ -4,3 +4,9 @@ if biscuits >= 5:
     print(f"We have {biscuits} biscuits.")
     print("It's time for tea!")
     print(f"We can invite {people} guests!")
+
+pastries = "chocolate chip cookies, scones, donuts"
+my_pastry = "scones"
+
+if my_pastry in pastries:
+    print(f"They have {my_pastry}!")
