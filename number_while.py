96901290d0cd233773c3a3ac0be9e8a385810bd3
Albert Ramos <albertramos.dev@gmail.com>
2021-12-14 12:04:59 -0700

    Add file

---
+++ number_while.py
@@ -0,0 +1,5 @@
+number = 0
+while number < 5:
+    print(number)
+    number += 1
+print(f"Now the number is equal to {number}")
---
+++ plurals.py
@@ -0,0 +1,7 @@
+number = 1
+word = 'engineer'
+
+if number > 1:
+    word += 's'
+
+print(number, word)
