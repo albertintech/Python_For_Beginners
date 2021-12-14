0fc6b9276279f466a58eeee4bd181a16f993e016
Albert Ramos <albertramos.dev@gmail.com>
2021-12-14 12:24:34 -0700

    Update number_while.py

--- number_while.py
+++ number_while.py
@@ -1,5 +1,25 @@
-number = 0
-while number < 5:
-    print(number)
-    number += 1
-print(f"Now the number is equal to {number}")
+# number = 0
+# while number < 5:
+#     print(number)
+#     number += 1
+# print(f"Now the number is equal to {number}")
+
+# i = 1
+# while i <= 20:
+#     print(i * i)
+#     i += 1
+
+# i = 0
+# a = 'a'
+# while i < 8:
+#     a *= 2
+#     i += 1
+# print(a)
+
+k = 4
+sum = 0
+i = 1
+while i <= k:
+    sum += i
+    i += 1
+print(sum)
