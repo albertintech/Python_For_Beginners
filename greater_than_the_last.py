dc751a681930122a13774c5d969564a531441acd
Albert Ramos <albertramos.dev@gmail.com>
2021-12-14 09:14:08 -0700

    Add new file

---
+++ greater_than_the_last.py
@@ -0,0 +1,5 @@
+first = int(input("Provide first number: "))
+second = int(input("Provide second number: "))
+third = int(input("Provide third number: "))
+result = first < second < third
+print(f"The three values provided are in ascending order: {str(result)}")
