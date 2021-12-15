bf96f55cc0c0d3c58bb60934101b61942f5e14ee
Albert Ramos <albertramos.dev@gmail.com>
2021-12-15 11:59:06 -0700

    Add Stage 3 feature

---
+++ projects/simple_chatty_bot.py
@@ -0,0 +1,23 @@
+# Project: Simple Chatty Bot
+
+# Stage 1: Introduce the Bot
+bot_name = 'Aid'
+birth_year = 2020
+
+print(f"Hello! My name is {bot_name}.")
+print(f"I was created in {birth_year}")
+
+# Stage 2: Print Your Name
+print("Please, remind me your name.")
+your_name = input()
+print(f"What a great name you have, {your_name}!")
+
+# Stage 3: Guess Age Feature
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5, and 7.")
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print(f"Your age is {your_age}; that's a good time to start programming!")
+
