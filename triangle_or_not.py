e6d13bdc5ec5de64b0ea688ec2ed03e9731875f2
Albert Ramos <albertramos.dev@gmail.com>
2021-12-18 14:52:48 -0700

    Add file

---
+++ triangle_or_not.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+triangle = 180
+
+if a + b + c == triangle:
+    print("The triangle is valid!")
+else:
+    print("The triangle is not valid!")
