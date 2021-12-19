ca10470e52dbbd3375b50ebf62cb70a2e592f073
Albert Ramos <albertramos.dev@gmail.com>
2021-12-19 10:35:16 -0700

    Refactor functions

--- projects/honest_calculator.py
+++ projects/honest_calculator.py
@@ -1,4 +1,4 @@
-# Honest Calculator
+# My Solution
 
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
@@ -6,39 +6,80 @@ msg_2 = "Yes ... an interesting math operation. You've slept through all classes
 msg_3 = "Yeah... division by zero. Smart move..."
 msg_4 = "Do you want to store the result? (y / n):"
 msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = " You are"
 operators = ['+', '-', '*', '/']
 memory = 0
 
 
+def is_one_digit(v):
+    if (v > -10 and v < 10) and isinstance(v, int) == True:
+        return True
+    else:
+        return False
+
+
+def check(v1, v2, v3):
+    msg = ""
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg = msg + msg_6
+    if (v1 == 1 or v2 == 1) and v3 == "*":
+        msg = msg + msg_7
+    if (v1 == 0 or v2 == 0) \
+            and (v3 == "*" or v3 == "+" or v3 == "-"):
+        msg = msg + msg_8
+    if msg != "":
+        msg = msg_9 + msg
+        print(msg)
+
 while True:
     print(msg_0)
-    equation = input().split()
-    x, oper, y = equation[0], equation[1], equation[2]
+    calc = input().split()
+    x, oper, y = calc[0], calc[1], calc[2]
     try:
-        x = float(equation[0])
-        y = float(equation[2])
-        oper = equation[1]
+        if x != 'M':
+            x = float(x)
+        else:
+            x = memory
+        if y != 'M':
+            y = float(y)
+        else:
+            y = memory
     except ValueError:
         print(msg_1)
-        continue # to the next lines of code
+        continue
 
-    if oper not in operators:
-        print(msg_2)
-        continue # to the next lines of code
+    check(x, y, oper)
 
-    if oper == '+':
-        print(x + y)
-        break # the loop and end program
-    elif oper == '-':
-        print(x - y)
-        break # the loop and end program
-    elif oper == '*':
-        print(x * y)
-        break # the loop and end program
-    elif oper == '/' and y != 0:
-        print(x / y)
-        break
+    if oper in operators:
+        if oper == '+':
+            result = float(x) + float(y)
+        elif oper == '-':
+            result = float(x) - float(y)
+        elif oper == '*':
+            result = float(x) * float(y)
+        elif oper == '/':
+            if y != 0:
+                result = float(x) / float(y)
+            else:
+                print(msg_3)
+                continue
+        print(result)
+
+        print(msg_4)
+        store_result = input()
+        if store_result == 'y':
+            memory = result
+
+        print(msg_5)
+        continue_calc = input()
+        if continue_calc == 'y':
+            continue
+        elif continue_calc == 'n':
+            break
     else:
-        print(msg_3) # go back to loop start
-        
+        print(msg_2)
+ 
 
