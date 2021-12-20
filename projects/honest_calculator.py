2b8f2267d4f9fd47afc0969ab1e7f1c5b3fee7f2
Albert Ramos <albertramos.dev@gmail.com>
2021-12-19 17:28:43 -0700

    Update file

--- projects/honest_calculator.py
+++ projects/honest_calculator.py
@@ -1,38 +1,41 @@
-msg_0 = "Enter an equation"
-msg_1 = "Do you even know what numbers are? Stay focused!"
-msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
-msg_3 = "Yeah... division by zero. Smart move..."
-msg_4 = "Do you want to store the result? (y / n):"
-msg_5 = "Do you want to continue calculations? (y / n):"
-msg_6 = " ... lazy"
-msg_7 = " ... very lazy"
-msg_8 = " ... very, very lazy"
-msg_9 = " You are"
+msg = [
+    "Enter an equation",
+    "Do you even know what numbers are? Stay focused!",
+    "Yes ... an interesting math operation. You've slept through all classes, haven't you?",
+    "Yeah... division by zero. Smart move...",
+    "Do you want to store the result? (y / n):",
+    "Do you want to continue calculations? (y / n):",
+    " ... lazy",
+    " ... very lazy",
+    " ... very, very lazy",
+    " You are",
+    "Are you sure? It is only one digit! (y / n)",
+    "Don't be silly! It's just one number! Add to the memory? (y / n)",
+    "Last chance! Do you really want to embarrass yourself? (y / n)"
+]
 operators = ['+', '-', '*', '/']
 memory = 0
 
-
 def is_one_digit(v):
-    if (v > -10 and v < 10) and isinstance(v, int) == True:
+    if (v > -10 and v < 10) and (v - int(v) == 0):  # ex 5.0 - 5 = 0
         return True
     else:
         return False
 
-
 def check(v1, v2, v3):
-    msg = ""
+    _msg = ""
     if is_one_digit(v1) and is_one_digit(v2):
-        msg = msg + msg_6
+        _msg = _msg + msg[6]  # "  ... lazy"
     if (v1 == 1 or v2 == 1) and v3 == "*":
-        msg = msg + msg_7
+        _msg = _msg + msg[7]  # " ... very lazy"
     if (v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-"):
-        msg = msg + msg_8
-    if msg != "":
-        msg = msg_9 + msg
-        print(msg)
+        _msg = _msg + msg[8]  # "... very, very lazy"
+    if _msg != "":
+        _msg = msg[9] + _msg  # "You are" + prior msg
+        print(_msg)
 
 while True:
-    print(msg_0)
+    print(msg[0])
     calc = input().split()
     x, oper, y = calc[0], calc[1], calc[2]
     try:
@@ -45,7 +48,7 @@ while True:
         else:
             y = memory
     except ValueError:
-        print(msg_1)
+        print(msg[1])  # "Do you even know what numbers are? Stay focused!"
         continue
     else:
         if oper in operators:
@@ -59,21 +62,21 @@ while True:
             elif oper == '/' and y != 0:
                 result = float(x) / float(y)
             else:
-                print(msg_3)
+                print(msg[3])  # "Yeah... division by zero. Smart move..."
                 continue
             print(result)
 
             while True:
-                print(msg_4)
+                print(msg[4])  # "Do you want to store the result? (y / n):"
                 ans = input()
-                if ans == 'y':
+                if ans == 'y': # Add additional flowchart logic here
                     memory = result
                     break
                 elif ans == 'n':
                     break
             
             while True:
-                print(msg_5)
+                print(msg[5])  # "Do you want to continue calculations? (y / n):"
                 cont_calc = input()
                 if cont_calc in ['n', 'y']:
                     break
@@ -83,7 +86,8 @@ while True:
             else:
                 continue
         else:
-            print(msg_2)
+            # "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+            print(msg[2])
             continue
  
 
